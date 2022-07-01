b61ab26637d0ba2cf5bae6ce0c33f5ea1f09992a
Jamin Echols <55197244+justjammin@users.noreply.github.com>
2022-07-01 18:38:37 +0000

    program works, had to fix the numberofMeals portion since it wasn't ending

--- main.py
+++ main.py
@@ -26,7 +26,15 @@ def cookingWithEchols(meals, sides, numberOfDinners, cook):
           mealsForThePeriod.pop()
           mealsForThePeriod.append(meal)
           print(*mealsForThePeriod, sep = "\n")
-          doYouLoveMe(mealsForThePeriod, mealChoice, sideChoice, food, meal, meals, sides)
+          if len(mealsForThePeriod) == numberOfDinners:
+            print("\n")
+            print("That's your Shopping List")
+            print(*mealsForThePeriod, sep = "\n")
+            with open('shopping_list.txt', 'w') as doc:
+              for item in mealsForThePeriod:
+                doc.write(item+ '\n')
+          else:
+            doYouLoveMe(mealsForThePeriod, mealChoice, sideChoice, food, meal, meals, sides)
         else: 
           print("I chose " + mealsForThePeriod[-1])
           mealChoice,sideChoice = mealsForThePeriod[-1].split(' & ')
@@ -36,7 +44,15 @@ def cookingWithEchols(meals, sides, numberOfDinners, cook):
           mealsForThePeriod.pop()
           mealsForThePeriod.append(meal)
           print(*mealsForThePeriod, sep = "\n")
-          doYouLoveMe(mealsForThePeriod, mealChoice, sideChoice, food, meal, meals, sides)
+          if len(mealsForThePeriod) == numberOfDinners:
+            print("\n")
+            print("That's your Shopping List")
+            print(*mealsForThePeriod, sep = "\n")
+            with open('shopping_list.txt', 'w') as doc:
+              for item in mealsForThePeriod:
+                doc.write(item+ '\n')
+          else:
+            doYouLoveMe(mealsForThePeriod, mealChoice, sideChoice, food, meal, meals, sides)
       else:
         print("nice, go me here's what we got")
         print(mealsForThePeriod, sep = "\n")
@@ -45,14 +61,17 @@ def cookingWithEchols(meals, sides, numberOfDinners, cook):
         meal= food.title()
         mealsForThePeriod.append(meal)
         print(*mealsForThePeriod, sep = "\n")
-        doYouLoveMe(mealsForThePeriod, mealChoice, sideChoice, food, meal, meals, sides)
+        if len(mealsForThePeriod) == numberOfDinners:
+          print("\n")
+          print("That's your Shopping List")
+          print(*mealsForThePeriod, sep = "\n")
+          with open('shopping_list.txt', 'w') as doc:
+            for item in mealsForThePeriod:
+              doc.write(item+ '\n')
+        else:
+          doYouLoveMe(mealsForThePeriod, mealChoice, sideChoice, food, meal, meals, sides)
     mealsForThePeriod.append(meal)
     print(*mealsForThePeriod, sep = "\n")
     shoppingList = doYouLoveMe(mealsForThePeriod, mealChoice, sideChoice, food, meal, meals, sides)
-  print("That's your Shopping List")
-  print(*mealsForThePeriod, sep = "\n")
-  with open('shopping_list.txt', 'w') as doc:
-    for item in mealsForThePeriod:
-      doc.write(item+ '\n')
 cookingWithEchols(['taquitos','chicken tenders','kievs','soups','potstickers','pork buns' 'chicken','roasted veggies', 'seafood' ,'stir fry','tacos','tofu','hamburgers','hot dogs','sloppy joe','pulled pork','salmon','bacon','shredded beef', 'pot roast', 'corned beef hash', 'spaghetti', 'katsu','stews','hamburg steak', 'burritos','empanadas','egg','onigiri','buns','loco moco', 'meatballs','meatloaf','ravioli'], ['white rice','salad','italian pasta','soups','asian pasta','egg rolls','veggies', 'quinoa salad','fried rice', 'yellow rice','fries', 'asparagus','brussel sprouts', 'waffles','potatoes', 'mashed potatoes', 'garlic bread'], 8, ["Jamin","Elaine"])
 #todo MainDishes json and sideDishs json to shorten this
